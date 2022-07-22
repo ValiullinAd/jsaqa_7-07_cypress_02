4aea7284f5f654f13441132e0f1f5b82429d0f76
ValiullinAd <93069873+ValiullinAd@users.noreply.github.com>
2022-07-22 19:08:36 +0300

    Update api.spec.js

--- cypress/e2e/api.spec.js
+++ cypress/e2e/api.spec.js
@@ -1,21 +1,72 @@
-it('API test', ()=> {
+describe("API test", () => {
+  it("Should post a new user", () => {
 
-cy.request({
-    method: "POST",
-    url: "https://petstore.swagger.io/v2/pet",
-    body: {
-      id: 1,
-      category: {id: 0, name: "string"},
-      name: "bobik",
-      photoUrls: ["string"],
-      tags: [{
-          id: 0,
-          name: "string",
-        }],
-      status: "available",
-    }
-  }).then((response) => {
-    cy.log(JSON.stringify(response.body));
-    expect(response.status).to.eq(200);
-}) 
-})
+      cy.createUser({
+          id: 453678,
+          username: "TestUser001",
+          firstName: "Vasilij",
+          lastName: "Podushkin",
+          email: "VPodushku@email.pl",
+          password: "1q2w3e",
+          phone: "+2378987678",
+          userStatus: 2,
+      }).then((response) => {
+          cy.log(JSON.stringify(response.body));
+          expect(response.status).to.eq(200);
+      });
+  });
+
+  it("Should put updated user", () => {
+  
+    cy.createUser({
+        id: 453678,
+        username: "UpdateUser001",
+        firstName: "Dasha",
+        lastName: "Marfushkina",
+        email: "DashMarf@mail.kr",
+        password: "1w2e3r",
+        phone: "7635564888",
+        userStatus: 2,
+    });
+
+    cy.request({
+        method: "PUT",
+        url: "https://petstore.swagger.io/v2/user/TestUser001",
+        body: {
+            id: 453678,
+            username: "UpdateUser001",
+            firstName: "Dasha",
+            lastName: "Vagina",
+            email: "DashVag@mail.kr",
+            password: "123qweasd",
+            phone: "7635564888",
+            userStatus: 1,
+        },
+    }).then((response) => {
+        cy.log(JSON.stringify(response.body));
+        expect(response.status).to.eq(200);
+    });
+});
+
+it("Should deleted user", () => {
+    
+    cy.createUser({
+        id: 453678,
+        username: "ShouldDeleteUser",
+        firstName: "Mihail",
+        lastName: "Tort",
+        email: "Tort@mail.kr",
+        password: "0987654",
+        phone: "223445566778",
+        userStatus: 2,
+    });
+
+    cy.request({
+        method: "DELETE",
+        url: "https://petstore.swagger.io/v2/user/ShouldDeleteUser",
+    }).then((response) => {
+        cy.log(JSON.stringify(response.body));
+        expect(response.status).to.eq(200);
+    });
+});
+}); 
